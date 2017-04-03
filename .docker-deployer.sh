1c8e082fa19d76a6d7cf8ef38e6892f1ac2a59a4
Dermot Byrne <dermotbyrne@dermot.local>
2017-04-03 16:53:23 +0100

    Reintroducing dit_env to see if it effects build stalling issue

--- .docker-deployer.sh
+++ .docker-deployer.sh
@@ -7,13 +7,16 @@ IIGB_BUILD=`date +%s`
 
 DOCKER_IMAGE="$DOCKER_IMAGE_NAME-deployer"
 
+echo "DIT ENV"
+echo $DIT_ENV
+
 echo "Attempting to delete previous run image if available"
 
 $DOCKER_PATH rmi -f $DOCKER_IMAGE || echo "No image found from previous build"
 
 $DOCKER_PATH images && \
 $DOCKER_PATH build --force-rm -t $DOCKER_IMAGE $PWD  && \
-$DOCKER_PATH run --rm -i --name $DOCKER_IMAGE -e DIT_ENV=$DIT_ENV -e IIGB_BUILD=$IIGB_BUILD -e BUCKET=$BUCKET -e DID=$DID -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e GIT_ACCESS_TOKEN=$GIT_ACCESS_TOKEN -e NO_ROBOTS=$NO_ROBOTS -e NO_CACHE=$NO_CACHE $DOCKER_IMAGE /bin/bash -c "npm run deploy"
+$DOCKER_PATH run --rm -i --name $DOCKER_IMAGE -e IIGB_BUILD=$IIGB_BUILD -e BUCKET=$BUCKET -e DID=$DID -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e GIT_ACCESS_TOKEN=$GIT_ACCESS_TOKEN -e NO_ROBOTS=$NO_ROBOTS -e NO_CACHE=$NO_CACHE $DOCKER_IMAGE /bin/bash -c "npm run deploy"
 
 result=$?
 
