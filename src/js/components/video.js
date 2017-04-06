b234f5776b862ce177239d7eee5d56bcb2a614c9
Dermot Byrne <dermotbyrne@dermot.local>
2017-04-06 15:29:35 +0100

    CSS Fix and Video fix

--- src/js/components/video.js
+++ src/js/components/video.js
@@ -33,3 +33,24 @@ var video = (function (window, $) {
 	};
 
 })(window, jQuery);
+
+/*
+  var tag = document.createElement('script');
+
+  tag.src = 'https://www.youtube.com/iframe_api'
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  var player;
+  function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+      height: '315',
+      width: '560',
+      videoId: 'S8CF_Mo3X-M'
+    });
+  }
+
+ jQuery(document).on('closed', '.remodal', function (e) {
+    player.stopVideo()
+  })
+*/
